589b7cbecdacae163191f7c5493c1f90007852b0
Victor Garcia Fernandez <victorgf2011@gmail.com>
2023-01-11 17:04:53 +0100

    remove debug

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,6 @@
   <main class="index">
     <h1>Hi, im <span class="text--tortitas">Tortitas</span></h1>
     <h2>Web developer, Indie game developer, <span class="text--deno">Deno</span> enthusiast.</h2>
-
-    <!-- <DebugScroll /> -->
-
   </main>
 </template>
 
